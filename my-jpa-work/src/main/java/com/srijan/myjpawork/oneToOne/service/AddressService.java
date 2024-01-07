e1c43765d2269c904594f11e0483e9bd3f646b31
venkatram64 <venkat.veerareddy@hotmail.com>
2024-01-07 09:56:26 +0530

    init commit

--- my-jpa-work/src/main/java/com/srijan/myjpawork/oneToOne/service/AddressService.java
+++ my-jpa-work/src/main/java/com/srijan/myjpawork/oneToOne/service/AddressService.java
@@ -1,7 +1,10 @@
 package com.srijan.myjpawork.oneToOne.service;
 
+import com.srijan.myjpawork.exception.MyCommonApiException;
 import com.srijan.myjpawork.oneToOne.model.Address;
+import com.srijan.myjpawork.oneToOne.model.Employee;
 import com.srijan.myjpawork.oneToOne.repository.AddressRepository;
+import com.srijan.myjpawork.oneToOne.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,27 @@ public class AddressService {
 
     @Autowired
     private AddressRepository addressRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Address createAddress(Address address){
+        Employee employee = address.getEmployee();
+        if(employee == null){
+            throw new MyCommonApiException("Employee object should not null");
+        }
+        Address newAddress = addressRepository.save(address);
+        return newAddress;
+    }
+
+    public Address updateAddress(Address address){
+        Employee employee = address.getEmployee();
+        if(employee == null){
+            throw new MyCommonApiException("Employee object should not null");
+        }
+        Address newAddress = addressRepository.save(address);
+        return newAddress;
+    }
 
     public List<Address> getAllAddress(){
         return addressRepository.findAll();
---
+++ my-jpa-work/src/test/java/com/srijan/myjpawork/oneToOne/service/AddressServiceTest.java
@@ -0,0 +1,90 @@
+package com.srijan.myjpawork.oneToOne.service;
+
+import com.srijan.myjpawork.oneToOne.model.Address;
+import com.srijan.myjpawork.oneToOne.model.Employee;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class AddressServiceTest {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Test
+    public void employeeAndAddressCreateTest(){
+
+        Employee employee = new Employee();
+        employee.setEmail("srijan.veerareddy@gmail.com");
+        employee.setFirstName("Srijan");
+        employee.setLastName("Veerareddy");
+        employee.setEmpNo("100");
+
+        Address address = new Address();
+        address.setEmployee(employee);
+        address.setLine1("Austin");
+        address.setCity("Austin");
+        address.setZipCode("123345");
+        address.setCity("US");
+        address.setCountry("Texus");
+
+        Address newAddress = addressService.createAddress(address);
+
+        Employee newEmp = newAddress.getEmployee();
+
+        List<Address> addressList = addressService.getAllAddress();
+
+        assert newAddress != null;
+        assert newEmp != null;
+        assert addressList != null;
+
+    }
+
+    @Test
+    public void employeeAndAddressUpdateTest(){
+        //creating
+        Employee employee = new Employee();
+        employee.setEmail("srijan.veerareddy@gmail.com");
+        employee.setFirstName("Srijan");
+        employee.setLastName("Veerareddy");
+        employee.setEmpNo("100");
+
+        Address address = new Address();
+        address.setEmployee(employee);
+        address.setLine1("Austin");
+        address.setCity("Austin");
+        address.setZipCode("123345");
+        address.setCity("US");
+        address.setCountry("Texus");
+
+        Address newAddress = addressService.createAddress(address);
+
+        Employee newEmp = newAddress.getEmployee();
+
+        List<Address> addressList = addressService.getAllAddress();
+
+        assert newAddress != null;
+        assert newEmp != null;
+        assert addressList != null;
+
+        //updating
+        newEmp.setEmpNo("7887");
+        Address updatedAddress = addressService.updateAddress(newAddress);
+
+        Employee updatedEmp = updatedAddress.getEmployee();
+        assertEquals(updatedEmp.getEmpNo(),"7887");
+
+    }
+
+}
